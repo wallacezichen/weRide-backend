189618040eec40a1035e00a929903721fb2bc386
Chaohao Du <chaohaodu@gmail.com>
2022-11-17 18:09:03 -0800

    update .gitignore

--- src/main/java/com/weride/repository/UserRepository.java
+++ src/main/java/com/weride/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.weride.repository;
 
+import com.weride.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public class UserRepository extends JpaRepository<User, Long> {
 }
